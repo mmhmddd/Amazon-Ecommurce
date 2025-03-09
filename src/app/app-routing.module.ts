a84c6633c253b03c509014029194f4904b754478
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-03-09 22:50:33 +0200

    finished my work dynamically from json file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { SingleProductsComponent } from './single-products/single-products.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LogInComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  {path:'details',component:SingleProductsComponent},
   { path: '**', component: HomeComponent }
 
 ];
--- src/app/single-products/single-products.component.ts
+++ src/app/single-products/single-products.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetourDataService } from '../services/getour-data.service';
 
 @Component({
   selector: 'app-single-products',
   templateUrl: './single-products.component.html',
   styleUrls: ['./single-products.component.scss']
 })
-export class SingleProductsComponent {
+export class SingleProductsComponent implements OnInit{
+  data:any;
+  product:any;
+  constructor(private fetchdata:GetourDataService){}
+ngOnInit(): void {
+  this.fetchdata.getData().subscribe((response)=>{
+    const data=response;
+    if (data.length > 0) {
+      this.product = data[0]; // Get the first product
+    }
+    console.log(this.data); // for checking
+  })
+}
 
 }
