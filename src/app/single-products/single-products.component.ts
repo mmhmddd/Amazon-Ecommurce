1f4976877f2b946b9e9069914f9506cdde235d20
Reham-Saeed <rehamsaeed074@gmail.com>
2025-03-14 10:50:29 +0200

    update

--- src/app/single-products/single-products.component.ts
+++ src/app/single-products/single-products.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { GetourDataService } from '../services/getour-data.service';
 import { CartService } from '../services/cart.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-products',
   templateUrl: './single-products.component.html',
-  styleUrls: ['./single-products.component.scss']
+  styleUrls: ['./single-products.component.scss'],
 })
-export class SingleProductsComponent implements OnInit{
-  data:any;
-  product:any;
-  constructor(private fetchdata:GetourDataService,private _CartService:CartService){}
-ngOnInit(): void {
-  this.fetchdata.getData().subscribe((response)=>{
-    const data=response;
-    if (data.length > 0) {
-      this.product = data[0]; // Get the first product
-    }
-    console.log(this.data); // for checking
-  })
-}
-addToCart(productId: number): void {
-  this._CartService.addToCart(productId);
-}
+export class SingleProductsComponent implements OnInit {
+  data: any;
+  product: any;
+  constructor(
+    private fetchdata: GetourDataService,
+    private _CartService: CartService,
+    private _ActivatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this._ActivatedRoute.paramMap.subscribe({
+      next: (param) => {
+        const id = param.get('id');
+        if (id) {
+          this.fetchdata.getData().subscribe((response) => {
+            const data = response;
+            this.product = data.find(
+              (item: any) => item.id === id || item.id === Number(id)
+            );
+          });
+        }
+      },
+    });
+  }
+
+  addToCart(productId: number): void {
+    this._CartService.addToCart(productId);
+  }
 }
